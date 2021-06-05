5bbdfd911446d06cd57e46b5ed2d38336ff86e1b
Steven Tsang <tsangste@gmail.com>
2021-06-05 12:44:12 +0100

    add initial websocket code

--- lib/aws-cdk-playground-stack.ts
+++ lib/aws-cdk-playground-stack.ts
@@ -1,9 +1,53 @@
-import * as cdk from '@aws-cdk/core';
+import * as path from 'path'
+import * as cdk from '@aws-cdk/core'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
+import * as lambda from '@aws-cdk/aws-lambda'
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
+import { WebSocketApi, WebSocketStage } from '@aws-cdk/aws-apigatewayv2'
+import { LambdaWebSocketIntegration } from '@aws-cdk/aws-apigatewayv2-integrations'
 
 export class AwsCdkPlaygroundStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    const connections = new dynamodb.Table(this, 'connectionsTable', {
+      tableName: 'connectionsTable',
+      partitionKey: { name: 'connectionId', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    const onConnect = new NodejsFunction(this, 'onConnect', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handle',
+      entry: path.join(__dirname, `/../src/onConnect/index.ts`),
+      environment: {
+        TABLE_NAME: 'connectionsTable'
+      }
+    })
+
+    connections.grantWriteData(onConnect)
+
+    const onDisconnect = new NodejsFunction(this, 'onDisconnect', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handle',
+      entry: path.join(__dirname, `/../src/onDisconnect/index.ts`),
+      environment: {
+        TABLE_NAME: 'connectionsTable'
+      }
+    })
+
+    connections.grantWriteData(onDisconnect)
+
+    const webSocketApi = new WebSocketApi(this, 'socket-gateway', {
+      connectRouteOptions: { integration: new LambdaWebSocketIntegration({ handler: onConnect }) },
+      disconnectRouteOptions: { integration: new LambdaWebSocketIntegration({ handler: onDisconnect }) }
+    })
+
+    new WebSocketStage(this, 'dev', {
+      webSocketApi,
+      stageName: 'dev',
+      autoDeploy: true
+    })
   }
 }
---
+++ src/onDisconnect/index.ts
@@ -0,0 +1,26 @@
+import * as AWS from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION })
+
+export async function handle(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+  const tableName = process.env.TABLE_NAME
+  if (!tableName) {
+    throw new Error('Table Name not Set')
+  }
+
+  const deleteParams = {
+    TableName: tableName,
+    Key: {
+      connectionId: event.requestContext.connectionId
+    }
+  };
+
+  try {
+    await ddb.delete(deleteParams).promise()
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to disconnect: ' + JSON.stringify(err) }
+  }
+
+  return { statusCode: 200, body: 'Disconnected.' }
+}
